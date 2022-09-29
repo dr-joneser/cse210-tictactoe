7703707492f5c3c828ce7a562563cddaa8f3d936
dr-joneser <andrew.joneser1@gmail.com>
2022-09-29 13:10:28 -0600

    Create Board.cs

---
+++ Board.cs
@@ -0,0 +1,13 @@
+class Board {
+
+    List<string> spaces = new List<string>();
+
+    // public Board() {
+    //     List<string> gameboard = new List<string>();
+    //     for (int i = 1; i < 10; i++)
+    //     {
+    //         spaces.Add(Convert.ToString(i));
+    //     }
+    // }
+
+}
